0b1957fa3bf91d80c837dbb595e1856589cbe6a3
Tomson Li <tomson.li@wustl.edu>
2025-04-15 17:26:22 -0500

    test

--- _plugins/google-scholar-citations.rb
+++ _plugins/google-scholar-citations.rb
@@ -21,7 +21,8 @@ module Jekyll
       article_id = context[@article_id.strip]
       scholar_id = context[@scholar_id.strip]
       article_url = "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=#{scholar_id}&citation_for_view=#{scholar_id}:#{article_id}"
-
+      GoogleScholarCitationsTag::Citations[u-x6o8ySG0sC] = 21
+      GoogleScholarCitationsTag::Citations[u5HHmVD_uO8C] = 3
       begin
           # If the citation count has already been fetched, return it
           if GoogleScholarCitationsTag::Citations[article_id]
