64b1099d61d25f39c574181d02a21a5b290eb92b
tomsonlee13 <tomson.li@wustl.edu>
2025-04-18 19:44:35 +0000

    Deploying to gh-pages from @ tomsonlee13/tomsonlee13.github.io@866d60e57f2c343bc59cbbf637a83a483df8ae4b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,18 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-image-galleries",
-        
-          title: "a post with image galleries",
-        
-        description: "this is what included image galleries could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/photo-gallery/";
-          
-        },
-      },{id: "post-a-post-with-redirect",
+        },{id: "post-a-post-with-redirect",
         
           title: "a post with redirect",
         
