f09fb60a7f8fdf6cbeea692da96ebb3e91b7df22
Tomson Li <tomson.li@wustl.edu>
2025-07-13 23:33:23 -0700

    update

--- _plugins/google-scholar-citations.rb
+++ _plugins/google-scholar-citations.rb
@@ -73,7 +73,7 @@ module Jekyll
         citation_count = "N/A"
 
         if article_id == "u-x6o8ySG0sC"
-          citation_count = "22"
+          citation_count = "35"
         elsif article_id == "u5HHmVD_uO8C"
           citation_count = "3"
         end
